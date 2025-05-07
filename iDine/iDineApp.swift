fc2ea71dffa9c850a6aad1b4dea4522fb17669a0
imnotdruish <105814537+imnotdruish@users.noreply.github.com>
2025-05-07 09:08:20 -0400

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Dan June on 5/7/25.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
